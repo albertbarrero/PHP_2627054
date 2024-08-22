342b0de2b29ca2c8aa2c255da1be55c5c30f0b34
ABL <albertbarrero@gmail.com>
2024-08-22 18:45:07 -0500

    Con Arreglo

--- induccion/arreglo.php
+++ induccion/arreglo.php
@@ -3,34 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de 30 Datos</title>
+    <title>Document</title>
 </head>
 <body>
-    <h2>Ingrese 10 datos:</h2>
+    
+<!-- Formulario para la cantidad de campos -->
     <form action="" method="post">
-        <?php
-        // Generar 30 campos de entrada
-        for ($i = 1; $i <= 10; $i++) {
-            echo '<label for="dato' . $i . '">Dato ' . $i . ':</label><br>';
-            echo '<input type="text" id="dato' . $i . '" name="datos[]" <br><br>';
-        }
-        ?>
+        <label for="cant"></label>
+        <input type="number" name="cant" id="cant required">
         <input type="submit" value="Enviar">
     </form>
 
     <?php
-   
-        // Recoger los datos del formulario
-        $datos = $_POST['datos'];
+        if (isset($_POST['cant'])) {
+            $cant=$_POST['cant'];
 
-        // Mostrar los datos almacenados en el arreglo
-        echo "<h3>Datos ingresados:</h3>";
-        echo "<ul>";
-        foreach ($datos as $indice => $valor) {
-            echo "<li>Dato " . ($indice + 1).": " . $valor . "</li>";
+            echo '<form action="" method="post">';
+            for ($i=0; $i < $cant; $i++) { 
+                echo'<label for="inp'.$i.'">Campo '.$i.'</label>';
+                echo'<input type="number" id="inp'.$i.'" name="datos[]" required></br>';
+            };
+            echo'<input type="submit" value="Enviar">';
+            echo '</form>';
         }
-        echo "</ul>";
-    
+
+        if (isset($_POST['datos'])) {
+        $datos=$_POST['datos'];
+            echo '<ul>';
+        foreach ($datos as $key => $datos) {
+            echo '<li>  dato '.($key+1).': '.$datos.'</li></br>';
+        };
+
+    };
     ?>
+
+
 </body>
 </html>
